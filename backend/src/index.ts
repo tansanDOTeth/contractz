c7fdbfb284c1ac93a8645bda03f7fe65af973f0a
tansan.eth <92072158+tansanDOTeth@users.noreply.github.com>
2023-04-28 08:38:14 +0900

    feat: add proper request validation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import { ExpressValidator, validationResult } from "express-validator";
 import express, { Express, Request, Response } from "express";
 
 import ContractAbi from "./ContractAbi";
@@ -6,6 +7,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const { param } = new ExpressValidator({
+  isValidEthereumAddress: async (value: string) => {
+    if (!Web3.utils.isAddress(value)) {
+      throw new Error("Contract Address invalid");
+    }
+  },
+});
 
 const app: Express = express();
 const port = process.env.PORT;
@@ -22,21 +31,24 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.get("/contracts/:contractAddress/abi", (req: Request, res: Response) => {
-  const { contractAddress } = req.params;
-  if (!Web3.utils.isAddress(contractAddress)) {
-    return res
-      .status(400)
-      .json({ status: 400, message: "Contract Address invalid" });
+app.get(
+  "/contracts/:contractAddress/abi",
+  param("contractAddress").isValidEthereumAddress(),
+  (req: Request, res: Response) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      const { contractAddress } = req.params;
+      const contractAbi = new ContractAbi(contractAddress);
+      return contractAbi.fetchAbi().then((abi) =>
+        res.send({
+          contractAddress: contractAddress,
+          abi,
+        })
+      );
+    }
+    return res.status(400).send({ errors: result.array() });
   }
-  const contractAbi = new ContractAbi(contractAddress);
-  return contractAbi.fetchAbi().then((abi) =>
-    res.send({
-      contractAddress: contractAddress,
-      abi,
-    })
-  );
-});
+);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
