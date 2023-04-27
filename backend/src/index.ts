851e5a6e658fdf3769f264aa0656d48abd9c4347
tansan.eth <92072158+tansanDOTeth@users.noreply.github.com>
2023-04-27 20:42:55 +0900

    feat: add cors headers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,21 @@ import express, { Express, Request, Response } from "express";
 
 import ContractAbi from "./ContractAbi";
 import Web3 from "web3";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const corsOrigins = process.env.CORS_ORIGINS?.split(",");
+
+app.use(
+  cors({
+    origin: corsOrigins,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
@@ -29,4 +38,5 @@ app.get("/contracts/:contractAddress/abi", (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log("⚡️[server]: CORS Origins", corsOrigins);
 });
