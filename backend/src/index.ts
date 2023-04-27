8c344f3e6b241c1a2c2890c9eeb395e7d8ef1b37
tansan.eth <92072158+tansanDOTeth@users.noreply.github.com>
2023-04-27 20:35:49 +0900

    feat: implement abi endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from "express";
 
+import ContractAbi from "./ContractAbi";
+import Web3 from "web3";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,6 +11,20 @@ const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+app.get("/contracts/:contractAddress/abi", (req: Request, res: Response) => {
+  const { contractAddress } = req.params;
+  if (!Web3.utils.isAddress(contractAddress)) {
+    res.status(400).json({ status: 400, message: "Contract Address invalid" });
+  }
+  const contractAbi = new ContractAbi(contractAddress);
+  contractAbi.fetchAbi().then((abi) =>
+    res.send({
+      contractAddress: contractAddress,
+      abi,
+    })
+  );
 });
 
 app.listen(port, () => {
