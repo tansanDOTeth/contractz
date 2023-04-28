c3ee0be7d2f6fbb63df08000c37cbf1f1513b643
tansan.eth <92072158+tansanDOTeth@users.noreply.github.com>
2023-04-28 11:52:29 +0900

    feat: implement endpoint for running read functions

--- backend/src/ContractAbi.ts
+++ backend/src/ContractAbi.ts
@@ -1,7 +1,8 @@
+import { AbiItem, fromWei, toBN } from "web3-utils";
 import ContractAbiCache, { CacheMissError } from "./ContractAbiCache";
 
+import Web3 from "web3";
 import fetch from "node-fetch";
-import { json } from "stream/consumers";
 
 export default class ContractAbi {
   contractAddress: string;
@@ -23,12 +24,34 @@ export default class ContractAbi {
     return fetch(url);
   }
 
-  private readCache(): Promise<object> {
+  private readCache(): Promise<AbiItem[]> {
     return this.abiCache.getAbi().then(JSON.parse);
   }
 
   private writeCache(data: any) {
     this.abiCache.setAbi(data);
+  }
+
+  private findAbi(
+    abi: AbiItem[],
+    functionName: string,
+    arity: number
+  ): AbiItem | undefined {
+    return abi
+      .filter((abi: AbiItem) => abi.type === "function")
+      .find(
+        (abiItem: AbiItem) =>
+          abiItem.name === functionName && abiItem.inputs?.length === arity
+      );
+  }
+
+  getAbiFunction(
+    functionName: string,
+    arity: number
+  ): Promise<AbiItem | undefined> {
+    return this.fetchAbi().then((abi) =>
+      this.findAbi(abi, functionName, arity)
+    );
   }
 
   /* 
@@ -44,7 +67,7 @@ export default class ContractAbi {
         poll until abi is indexed
         return abi
   */
-  fetchAbi(): Promise<object> {
+  fetchAbi(): Promise<AbiItem[]> {
     try {
       return this.readCache();
     } catch (error: any) {
@@ -62,6 +85,73 @@ export default class ContractAbi {
           throw error;
         }
       }
+    }
+  }
+
+  runFunction(
+    functionName: string,
+    functionInputs: Array<string | number>,
+    formatResult = true
+  ) {
+    if (
+      !process.env.ETHEREUM_RPC_URL ||
+      process.env.ETHEREUM_RPC_URL.length === 0
+    ) {
+      throw new Error("Need Ethereum RPC");
+    }
+
+    return this.fetchAbi().then((abi) => {
+      const web3 = new Web3(process.env.ETHEREUM_RPC_URL!);
+      const Contract = web3.eth.Contract;
+      const contract = new Contract(abi, this.contractAddress);
+      const contractFunction = contract.methods[functionName];
+
+      return new Promise((resolve, reject) => {
+        contractFunction(...functionInputs).call((error: any, result: any) => {
+          if (error) {
+            return reject(error);
+          }
+          if (formatResult) {
+            const abiItem = this.findAbi(
+              abi,
+              functionName,
+              functionInputs.length
+            );
+            return resolve(this.formatResult(abiItem!, result));
+          }
+          return resolve(result);
+        });
+      });
+    });
+  }
+
+  private format(value: any, type: string) {
+    switch (type) {
+      case "uint256": {
+        return value;
+      }
+      default: {
+        console.log("Type unsupported and needs to be formatted", value, type);
+        return value;
+      }
+    }
+  }
+
+  private formatResult(abiItem: AbiItem, result: any) {
+    // TODO: Implement multiple return values
+    if (Array.isArray(result)) {
+      // Loop through outputs
+      console.log(
+        "Multiple return formatting not yet supported! Please add this!",
+        abiItem,
+        result
+      );
+      throw new Error(
+        "Multiple return formatting not yet supported! Please add this!"
+      );
+    } else {
+      const output = abiItem.outputs![0];
+      return this.format(result, output.type);
     }
   }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
-import { ExpressValidator, validationResult } from "express-validator";
+import {
+  ExpressValidator,
+  matchedData,
+  validationResult,
+} from "express-validator";
 import express, { Express, Request, Response } from "express";
 
+import { AbiItem } from "web3-utils";
 import ContractAbi from "./ContractAbi";
 import Web3 from "web3";
 import cors from "cors";
@@ -16,10 +21,27 @@ const { param } = new ExpressValidator({
   },
 });
 
+const { body } = new ExpressValidator({
+  isValidAbiFunction: async (functionName: string, { req }: { req: any }) => {
+    const contractAbi = new ContractAbi(req.params.contractAddress);
+    const abiItem = await contractAbi
+      .getAbiFunction(functionName, req.body.inputs.length)
+      .catch(() => {
+        throw new Error("Contract Address invalid");
+      });
+
+    if (!abiItem) {
+      throw new Error("Invalid function and/or arity for contract address");
+    }
+    return abiItem;
+  },
+});
+
 const app: Express = express();
 const port = process.env.PORT;
 const corsOrigins = process.env.CORS_ORIGINS?.split(",");
 
+app.use(express.json());
 app.use(
   cors({
     origin: corsOrigins,
@@ -37,7 +59,7 @@ app.get(
   (req: Request, res: Response) => {
     const result = validationResult(req);
     if (result.isEmpty()) {
-      const { contractAddress } = req.params;
+      const { contractAddress } = matchedData(req);
       const contractAbi = new ContractAbi(contractAddress);
       return contractAbi.fetchAbi().then((abi) =>
         res.send({
@@ -45,6 +67,39 @@ app.get(
           abi,
         })
       );
+    }
+    return res.status(400).send({ errors: result.array() });
+  }
+);
+
+app.post(
+  "/contracts/:contractAddress/run",
+  param("contractAddress").notEmpty().isValidEthereumAddress(),
+  body("type").custom((value) => value === "function"),
+  body("inputs").isArray(),
+  body("name").isValidAbiFunction(),
+  async (req: Request, res: Response) => {
+    const { contractAddress, name, inputs } = matchedData(req);
+    console.log("data", matchedData(req));
+
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      const contractAbi = new ContractAbi(contractAddress);
+      const abiItem = await contractAbi.getAbiFunction(name, inputs.length);
+      return contractAbi
+        .runFunction(
+          name,
+          inputs.map((input: any) => input.value)
+        )
+        .then((result: any) =>
+          res.send({
+            result,
+          })
+        )
+        .catch((error) => {
+          console.log(error);
+          return res.status(400).send();
+        });
     }
     return res.status(400).send({ errors: result.array() });
   }
